6aa21476e2746660ea8e31a766a3f8a40daa2154
Gowtham Kumar G <144571925+GowthamKumar-GK@users.noreply.github.com>
2026-08-06 04:21:51 +0530

    Update Powerplay Stats section header style

--- T20_Streamlit_App/t20_streamlit/app.py
+++ T20_Streamlit_App/t20_streamlit/app.py
@@ -355,7 +355,7 @@ st.markdown("""
 # ── INPUT CARD ────────────────────────────────────────────────
 st.markdown("""
 
-<div class="sec-lbl" style="font-weight: bold; font-size: 23px;">
+<div class="sec-lbl" style="font-weight: bold; font-size: 20px;">
     ⚡ Enter Powerplay Stats — Overs 1 to 6
 </div>
 </div>
